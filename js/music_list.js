47009dc131c89e399b36ba4886784007296bcc10
Luis Ron <116908552+LuisRon27@users.noreply.github.com>
2023-02-21 15:30:45 -0300

    maqueta

---
+++ js/music_list.js
@@ -0,0 +1,20 @@
+let allMusic = [
+{
+name: "Lasso - Diferente",
+artist: "Lasso",
+img: "lasso.jpg",
+src: "lasso"
+},
+{
+name: "Micro Tdh - dont get out",
+artist: "Micro Tdh",
+img: "micro",
+src: "micro"
+},
+{
+name: "kurt - los dias que vendran",
+artist: "kurt",
+img: "kurt",
+src: "kurt"
+}
+];
